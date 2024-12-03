af83973c31b1b9f6d0254dd505877c471f1ee07e
Emanueeelz <e.correia@escolar.ifrn.edu.br>
2024-12-03 20:36:40 -0300

    criação de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CabecalhoComponent} from './cabecalho/cabecalho.component'
+import {CorpoSiteComponent} from './corpo-site/corpo-site.component'
+import { RodapeComponent} from './rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabecalhoComponent, CorpoSiteComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
