bc1ac8e76d66c311279ad5b8e0eefc5092e104ef
Emanueeelz <e.correia@escolar.ifrn.edu.br>
2024-12-08 19:18:31 -0300

    se pa ta pronto, ou quase

--- src/app/rodape/rodape.component.ts
+++ src/app/rodape/rodape.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContatoComponent } from "../contato/contato.component";
 
 @Component({
   selector: 'app-rodape',
   standalone: true,
-  imports: [],
+  imports: [ContatoComponent],
   templateUrl: './rodape.component.html',
   styleUrl: './rodape.component.css'
 })
